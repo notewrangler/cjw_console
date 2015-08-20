6a51f58e4cfd87b90c7d3a012f0bc7bb3a0af7ff
ralph <noiseartist@gmail.com>
2015-08-19 17:01:01 -0700

    added grab_name callback to photo.rb

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,21 @@ class Photo < ActiveRecord::Base
 	belongs_to :product
 
 	mount_uploader :image, ImageUploader
+
+	before_save :grab_file_name
+
+	def grab_file_name
+		backword = self.image.to_s
+		bw = backword.reverse
+		word = ""
+		i = 0
+		bw.each_char do |c|
+			c = bw[i]
+		 	break if c == "/"
+		 	word += c 
+		 	i += 1
+		end 
+		self.name = word.reverse
+	end
+ 
 end
